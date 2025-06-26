6734e9bd8e566252ecb56c6209e0c8f1f6da3a80
bth <gh@hautogdoad.com>
2025-06-26 14:53:19 -0700

    add http handlers and fix components imports by updgrading to lastest templui CLI

--- go.mod
+++ go.mod
@@ -7,6 +7,7 @@ require (
 	github.com/a-h/templ v0.3.906
 	github.com/google/uuid v1.6.0
 	github.com/jackc/pgx/v5 v5.7.4
+	github.com/joho/godotenv v1.5.1
 )
 
 require (
--- main.go
+++ main.go
@@ -3,11 +3,53 @@ package main
 import (
 	"context"
 	"fmt"
+	"net/http"
+	"os"
 
 	"pgx-sqlc/db"
+	"pgx-sqlc/ui/assets"
+	"pgx-sqlc/ui/pages"
+
+	"github.com/a-h/templ"
+	"github.com/joho/godotenv"
 )
 
+func InitDotEnv() {
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println("Error loading .env file")
+	}
+}
+
+func SetupAssetsRoutes(mux *http.ServeMux) {
+	var isDevelopment = os.Getenv("GO_ENV") != "production"
+
+	assetHandler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if isDevelopment {
+			w.Header().Set("Cache-Control", "no-store")
+		}
+
+		var fs http.Handler
+		if isDevelopment {
+			fs = http.FileServer(http.Dir("./ui/assets"))
+		} else {
+			fs = http.FileServer(http.FS(assets.Assets))
+		}
+
+		fs.ServeHTTP(w, r)
+	})
+
+	mux.Handle("GET /ui/assets/", http.StripPrefix("/ui/assets/", assetHandler))
+}
+
 func main() {
+	InitDotEnv()
+	mux := http.NewServeMux()
+	SetupAssetsRoutes(mux)
+	mux.Handle("GET /", templ.Handler(pages.Landing()))
+	fmt.Println("Server is running on http://localhost:8090")
+	http.ListenAndServe(":8090", mux)
+
 	ctx := context.Background()
 	db_url := "user=bth database=testdb"
 	pgdb := db.NewDatabase(ctx, db_url)
@@ -24,9 +66,9 @@ func main() {
 	}
 	fmt.Println(newprod.Name)
 
-	products, err := pgdb.Query.ListProducts(ctx, joe.ID)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(products)
+	// products, err := pgdb.Query.ListProducts(ctx, joe.ID)
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// fmt.Println(products)
 }
