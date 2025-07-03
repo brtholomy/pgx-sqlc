1a897c1cc2991c550d8eb728cae55a7e32aaec05
bth <gh@hautogdoad.com>
2025-07-03 15:03:17 -0700

    try the db

--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"context"
 	"errors"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 
+	"pgx-sqlc/db"
 	"pgx-sqlc/qbo"
 	"pgx-sqlc/ui/assets"
 	"pgx-sqlc/ui/pages"
@@ -27,16 +29,14 @@ func initDotEnv() error {
 	return errors.New("Could not load .env file")
 }
 
-func setupAssetsRoutes(mux *http.ServeMux) {
-	var is_development = os.Getenv("GO_ENV") != "production"
-
+func setupAssetsRoutes(mux *http.ServeMux, isdev bool) {
 	asset_handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if is_development {
+		if isdev {
 			w.Header().Set("Cache-Control", "no-store")
 		}
 
 		var fs http.Handler
-		if is_development {
+		if isdev {
 			fs = http.FileServer(http.Dir("./ui/assets"))
 		} else {
 			fs = http.FileServer(http.FS(assets.Assets))
@@ -53,8 +53,12 @@ func main() {
 	if err := initDotEnv(); err != nil {
 		panic(err)
 	}
+	var isdev = os.Getenv("GO_ENV") != "production"
+
+	////////////////////////////////////////
+	// handlers
 	mux := http.NewServeMux()
-	setupAssetsRoutes(mux)
+	setupAssetsRoutes(mux, isdev)
 	mux.Handle("GET /", templ.Handler(pages.Landing()))
 
 	// http.Handler implementations:
@@ -77,29 +81,35 @@ func main() {
 	// mux.HandleFunc("GET /qbo", qbo.GetInvoiceFunc)
 	// mux.HandleFunc("POST /qbo", qbo.PostInvoiceFunc)
 
-	fmt.Println("Server is running on http://localhost:8090")
 	http.ListenAndServe(":8090", mux)
+	if isdev {
+		fmt.Println("Server is running on http://localhost:8090")
+	}
 
+	////////////////////////////////////////
 	// DB code:
-	// ctx := context.Background()
-	// db_url := "user=bth database=testdb"
-	// pgdb := db.NewDatabase(ctx, db_url)
+	ctx := context.Background()
+	db_url := "user=bth database=testdb"
+	if !isdev {
+		db_url = os.Getenv("RENDER_INTERNAL_DB")
+	}
+	pgdb := db.NewDatabase(ctx, db_url)
 
-	// // joe, err := db.NewUser(ctx, pgdb, "joe", "j@blow.com")
+	joe, err := db.NewUser(ctx, pgdb, "joe", "j@blow.com")
 	// joe, err := db.GetUser(ctx, pgdb, "0197ada4-5f8b-77d7-b039-5651eabf19e1")
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// udb := db.UserDatabase{&joe, pgdb}
-	// newprod, err := udb.NewProduct(ctx, "stuff", "3.15")
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// fmt.Println(newprod.Name)
+	if err != nil {
+		panic(err)
+	}
+	udb := db.UserDatabase{&joe, pgdb}
+	newprod, err := udb.NewProduct(ctx, "stuff", "3.15")
+	if err != nil {
+		panic(err)
+	}
+	log.Println(newprod.Name)
 
-	// products, err := pgdb.Query.ListProducts(ctx, joe.ID)
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// fmt.Println(products)
+	products, err := pgdb.Query.ListProducts(ctx, joe.ID)
+	if err != nil {
+		panic(err)
+	}
+	log.Println(products)
 }
